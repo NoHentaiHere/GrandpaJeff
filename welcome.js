b0363b1f8fb897389a352cae2a47186edbff1e2d
Andrew <54633122+NoHentaiHere@users.noreply.github.com>
2021-01-26 12:31:34 +0100

    Delete welcome.js

--- welcome.js
+++
@@ -1,9 +0,0 @@
-module.exports = client => {
-    const channelId = '765124850696060948'
-    client.on('guildMemberAdd', member => {   
-        console.log(member)
-      const message = `Dobrodosao <@${member.id}>`
-      const channel = member.guild.channels.cache.get(channelId)
-      channel.send(message)
-      })
-}
